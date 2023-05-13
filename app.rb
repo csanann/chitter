3e0f8b95720891acf316a5cfad89c64da7e86a12
csanann <csanann@gmail.com>
2023-05-13 16:26:59 +0100

    File setup

---
+++ app.rb
@@ -0,0 +1,11 @@
+#file: app.rb
+
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Application < Sinatra::Base
+
+    configure :development do
+        register Sinatra::Reloader
+    end
+end
