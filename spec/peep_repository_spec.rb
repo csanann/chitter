cbd380b66415ab3d8085fd2aab8e394373da2169
csanann <csanann@gmail.com>
2023-05-14 11:48:33 +0100

    Add spec/peep_repository_spec file

---
+++ spec/peep_repository_spec.rb
@@ -0,0 +1,55 @@
+#file: spec/peep_repository_spec.rb
+
+require 'spec_helper'
+require 'lib/peep_repository'
+
+
+RSpec.describe PeepRepository do
+      
+  before(:each)do
+    reset_all_tables
+  end
+    
+  
+  it 'returns a list of users' do
+      repo = PeepRepository.new
+      peeps = repo.all
+
+      expect(peeps.length).to eq(2)
+      expect(peeps[0].id).to eq(1).to_i
+      expect(peeps[0].message).to eq('My name is Ken YoohaaaYoo.')
+      expect(peeps[0].timestamp).to eq('2023-05-11 09:09:08')
+      expect(peeps[1].id).to eq(2).to_i
+      expect(peeps[1].message).to eq('What a lovely peep.')
+      expect(peeps[1].timestamp).to eq('2023-05-11 09:08:08')
+    end
+  end
+
+  it 'creates a new peep' do
+      peep = Peep.new
+      peep.message = ('What a lovely day!')
+      peep.timestamp = (Time.now)
+      peep.user_id = (2)
+
+      repo = PeepRepository.new
+      repo.create(peep)
+      peeps = repo.all
+
+      expect(peeps[-1].id).to eq(2).to_i
+      expect(peeps[-1].message).to eq('What a lovely day!')
+      expect(peeps[-1].timestamp).to eq(Time.now)
+      expect(peeps[-1].user_id).to eq(2)
+    end
+  end
+
+
+  it 'finds a peep by id' do
+      repo = PeepRepository.new
+      peep = repo.find_by_id(1)
+
+      expect(peep.id).to eq(1)
+      expect(peep.message).to eq('My name is Ken YoohaaaYoo.')
+      expect(peeps.timestamp).to eq('2023-05-11 09:09:08')
+      expect(peep.user_id).to eq(1)
+  end
+end
